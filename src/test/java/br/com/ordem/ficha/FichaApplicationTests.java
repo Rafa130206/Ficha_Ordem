bb03a44ecadb6b4e6bfc6936a513710a4b668087
Rafa130206 <rafabompadre@gmail.com>
2025-10-14 16:29:02 -0300

    primeiro commit

---
+++ src/test/java/br/com/ordem/ficha/FichaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ordem.ficha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
