55f6fc2a9eb67008618f75f47e6d12be0a99f2f9
Rafa130206 <rafabompadre@gmail.com>
2025-10-14 17:27:51 -0300

    UsuarioService criado

--- src/main/java/br/com/ordem/ficha/repository/UsuarioRepository.java
+++ src/main/java/br/com/ordem/ficha/repository/UsuarioRepository.java
@@ -1,4 +1,15 @@
 package br.com.ordem.ficha.repository;
 
-public class UsuarioRepository {
+import br.com.ordem.ficha.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
--- src/main/java/br/com/ordem/ficha/service/UsuarioService.java
+++ src/main/java/br/com/ordem/ficha/service/UsuarioService.java
@@ -1,4 +1,37 @@
 package br.com.ordem.ficha.service;
 
-public class UsuarioService {
+import br.com.ordem.ficha.model.Usuario;
+import br.com.ordem.ficha.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = repo.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        return User.withUsername(usuario.getEmail())
+                .password(usuario.getSenha())
+                .roles("USER")
+                .build();
+    }
+
+    public void salvar(Usuario usuario) {
+        if (repo.existsByEmail((usuario.getEmail()))) {
+            throw new IllegalArgumentException("E-mail já cadastrado");
+        }
+
+        usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+        repo.save(usuario);
+    }
+
 }
