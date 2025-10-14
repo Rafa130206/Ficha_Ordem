fc14018fffecd605fc836a0cfdd4a2ee258783d4
Rafa130206 <rafabompadre@gmail.com>
2025-10-14 16:38:44 -0300

    model Usuario criado

---
+++ src/main/java/br/com/ordem/ficha/model/Usuario.java
@@ -0,0 +1,21 @@
+package br.com.ordem.ficha.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+}
