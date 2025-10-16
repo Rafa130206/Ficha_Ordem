be575eb23523c5c96443f61fd95daa3c2336f36b
Rafa130206 <rafabompadre@gmail.com>
2025-10-15 22:37:21 -0300

    model Antecedentes criado

---
+++ src/main/java/br/com/ordem/ficha/model/Antecedentes.java
@@ -0,0 +1,25 @@
+package br.com.ordem.ficha.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Antecedentes {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricaoPessoal;
+    private String ideologias;
+    private String pessoasSignificativas;
+    private String locaisImportantes;
+    private String pertencesQueridos;
+    private String fobias;
+    private String passado;
+}
